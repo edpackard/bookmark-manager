0e091617f131011b07c9326ce3b78b04e41560af
zainab baruud <zainabbaruud@ma373.home>
2021-08-19 14:33:41 +0100

    fixing error

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -14,9 +14,9 @@ describe Bookmark do
     
       bookmarks = Bookmark.all
     
-      expect(bookmarks).to include "http://www.makersacademy.com/"
-      expect(bookmarks).to include "http://www.google.com/"
-      expect(bookmarks).to include "http://www.destroyallsoftware.com/"
+      expect(bookmarks).to include "http://www.makersacademy.com"
+      expect(bookmarks).to include "http://www.google.com"
+      expect(bookmarks).to include "http://www.destroyallsoftware.com"
     end
     
   end
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -7,7 +7,12 @@ end
 
 feature "Display list of bookmarks" do 
   scenario "makes get request to /bookmarks route" do 
+  connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    # Add the test data
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+
     visit "/bookmarks"
-    expect(page).to have_content("http://www.makersacademy.com/")
+    expect(page).to have_content("http://www.makersacademy.com")
   end 
 end 
